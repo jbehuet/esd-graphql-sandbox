8d33e4a6161ba1cddd669eb73cbd4f78a4da4e14
Jérôme BEHUET <jbehuet@gmail.com>
2022-10-27 11:18:32 +0200

    add REST api

--- server.js
+++ server.js
@@ -12,8 +12,47 @@ app.use(express.urlencoded({ extended: false }));
 /** Takes care of JSON data */
 app.use(express.json());
 
+/** DATA */
+let todos = [
+    {
+        id: 0,
+        task: "Faire les courses",
+        completed: false
+    }
+];
 
 
+/** REST API */
+app.get("/todos", (req, res) => {
+    res.send(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+    const todo = todos.find(todo => todo.id == req.params.id);
+    if (!todo) res.sendStatus(404);
+    res.send(todo);
+});
+
+app.post("/todos", (req, res) => {
+    todos.push({
+        id: todos.length,
+        task: req.body.task,
+        completed: false
+    });
+    res.sendStatus(201);
+});
+
+app.put("/todos/:id", (req, res) => {
+    const idx = todos.findIndex(todo => todo.id == req.params.id);
+    todos[idx] = req.body;
+    res.sendStatus(200);
+});
+
+
+app.delete("/todos/:id", (req, res) => {
+    todos = todos.filter(todo => todo.id != req.params.id);
+    res.sendStatus(200);
+});
 
 /** Server */
 const httpServer = http.createServer(app);
